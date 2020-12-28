13211330cf0c93c0619c356ec8c65f8dea725efc
Frankie Eder <frankaeder@gmail.com>
2020-12-27 22:38:37 -0800

    Build plugin to repository.

---
+++ Effect/VisEcho/Mac/Build/Intermediates.noindex/VisEcho.build/Debug/VisEcho.build/Objects-normal/x86_64/Echo.d
@@ -0,0 +1,26 @@
+dependencies: \
+  /Users/frankeder/Downloads/adobe_droste_echo/Effect/VisEcho/Echo.cpp \
+  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/Cocoa.framework/Headers/Cocoa.h \
+  /Users/frankeder/Downloads/adobe_droste_echo/Effect/VisEcho/Echo.h \
+  ../../../Headers/AEConfig.h ../../../Util/entry.h \
+  /Users/frankeder/Downloads/adobe_droste_echo/Headers/AE_PluginData.h \
+  ../../../Headers/A.h ../../../Headers/adobesdk/config/PreConfig.h \
+  ../../../Headers/adobesdk/config/PostConfig.h \
+  /Users/frankeder/Downloads/adobe_droste_echo/Headers/AE_Effect.h \
+  ../../../Headers/AE_EffectCB.h ../../../Headers/AE_Effect.h \
+  ../../../Headers/AE_Macros.h ../../../Headers/AE_ChannelSuites.h \
+  ../../../Headers/SP/SPBasic.h ../../../Headers/SP/SPTypes.h \
+  ../../../Headers/SP/SPConfig.h ../../../Headers/SP/SPErrorCodes.h \
+  ../../../Headers/AE_EffectSuites.h ../../../Headers/AE_EffectUI.h \
+  ../../../Headers/adobesdk/drawbotsuite/DrawbotSuiteTypes.h \
+  ../../../Headers/AE_EffectSuitesOld.h ../../../Util/Param_Utils.h \
+  ../../../Util/AEFX_SuiteHelper.h \
+  ../../../Headers/AEFX_SuiteHandlerTemplate.h \
+  ../../../Headers/SP/SPSuites.h ../../../Headers/SP/SPAccess.h \
+  ../../../Headers/SP/SPPlugs.h ../../../Headers/SP/SPFiles.h \
+  ../../../Headers/SP/SPProps.h ../../../Headers/SP/SPMData.h \
+  ../../../Headers/SP/SPPiPL.h ../../../Headers/SP/SPAdapts.h \
+  ../../../Headers/SP/SPCaches.h ../../../Headers/SP/SPStrngs.h \
+  ../../../Headers/adobesdk/DrawbotSuite.h \
+  ../../../Headers/adobesdk/config/AdobesdkTypes.h \
+  /Users/frankeder/Downloads/adobe_droste_echo/Util/DuckSuite.h
